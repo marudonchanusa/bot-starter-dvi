898dfb2b938245dbce759a8359ae9f5f76db4491
吉田 敬一 <yoshida@e-cometrue.net>
2018-08-28 17:48:47 +0900

    修正

--- index.php
+++ index.php
@@ -24,7 +24,8 @@
 
 		switch($event->getType()){
 			case 'follow':
-				joinFriend($bot, $event);
+				//joinFriend($bot, $event);
+				replyTextMessage($bot, $event->getReplyToken(), 'essage Recieve OK!');
 				break;
 			case 'message':
 				replyTextMessage($bot, $event->getReplyToken(), 'Message Recieve OK!');
