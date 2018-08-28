d8f88386afe2a0c087070789b3185796116f1de0
吉田 敬一 <yoshida@e-cometrue.net>
2018-08-28 18:12:55 +0900

    e

--- index.php
+++ index.php
@@ -36,10 +36,10 @@
 
 	// 友達追加時のイベント
 	function joinFriend($bot, $event) {
-		$returnStr = 'userId:' . $event->getUserId();
-		$returnStr.= 'グループID:' . $event->getGroupId();
-		$returnStr.= 'ルームID:' . $event->getRoomId();
-		$returnStr.= '友達追加して頂きありがとうございます。';
+		//$returnStr = 'userId:' . $event->getUserId();
+		//$returnStr.= 'グループID:' . $event->getGroupId();
+		//$returnStr.= 'ルームID:' . $event->getRoomId();
+		$returnStr = '友達追加して頂きありがとうございます。';
 		
 		replyTextMessage($bot, $event->getReplyToken(), $returnStr);
 	}
